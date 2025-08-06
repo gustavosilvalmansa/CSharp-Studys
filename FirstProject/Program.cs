cc91bfed7921045ea4eddda512d935632593b923
Gustavo Almansa <gustavosalmansa@gmail.com>
2025-08-06 15:15:27 -0300

    First IF

---
+++ FirstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            SByte x = 100;
+
+            if (x > 0)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("No arguments were passed.");
+            }
+        }
+    }
+
+
+
+}
